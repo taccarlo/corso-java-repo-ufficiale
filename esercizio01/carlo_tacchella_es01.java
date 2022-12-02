736131fd3ada07c85c6e53c265cc242ee1d427b0
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-02 09:48:08 +0100

    update hello world tacchella

--- esercizio01/carlo_tacchella_es01.java
+++ esercizio01/carlo_tacchella_es01.java
@@ -2,6 +2,6 @@
 
 class carlo_tacchella_es01 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Ciao a tutti mi chiamo Carlo!"); 
     }
 }

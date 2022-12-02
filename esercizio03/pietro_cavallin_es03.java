7726894748756db3b51a5974e6ad5445ae03c335
pietrocavallin <[200~69079102+pietrocavallin@users.noreply.github.com~>
2022-12-02 11:53:41 +0100

    edit 00_test.txto + add pietro_cavallin_es03

---
+++ esercizio03/pietro_cavallin_es03.java
@@ -0,0 +1,42 @@
+public class pietro_cavallin_es03 {
+    public static void main(String[] args)
+    {
+        esercizio01();
+        esercizio02();
+        esercizio03();
+        esercizio04();
+    }
+
+    /**
+     Leggere un array di interi di 10 posizioni
+     e stampare il numero che compare più volte all’interno dell’array,
+     qualora ci siano più numeri che compaiano lo stesso numero di volte stampare quello che compare per primo.
+     */
+    private static void esercizio01() {
+
+    }
+
+    /**
+     Leggere  un array di 10 interi e stampare solo i numeri che appaiono nell’array una volta soltanto.
+     Ad esempio se l’array contiene  1, 2, 3, 1, 2, 4  il programma stamperà  3, 4
+     */
+    private static void esercizio02() {
+
+    }
+
+    /**
+     Creare un array di interi con 8 posizione
+     e riempirlo con numeri casuali compresi tra 1 e 90 senza ripetizioni.
+     Stampare man mano il contenuto dell’array.
+     */
+    private static void esercizio03() {
+
+    }
+
+    /**
+     Eseguire un bubble sort su un array in input.
+     */
+    private static void esercizio04() {
+
+    }
+}

6784f6d1b1b4ecf45697e5b3c4454ab407beac22
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-02 12:25:29 +0100

    [ADD] esercizio 4 testo

---
+++ esercizio04/carlo_tacchella_es04.java
@@ -0,0 +1,15 @@
+package esercizio04;
+
+public class carlo_tacchella_es04 {
+    /** 
+        Write a program to print the area of a rectangle by creating a class 
+        named 'Area' having two methods. First method named as 'setDim' takes 
+        length and breadth of rectangle as parameters and the second method 
+        named as 'getArea' returns the area of the rectangle. Length and 
+        breadth of rectangle are entered through keyboard.
+    */
+
+    public static void main(String[] args) {
+       System.out.println("ciao");
+    }
+}

12da0b9ae35cd887ba5e6f3fe36c4b12a919226e
pietrocavallin <[200~69079102+pietrocavallin@users.noreply.github.com~>
2022-12-07 09:39:19 +0100

    edit

--- lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
+++ lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
@@ -15,8 +15,30 @@ public class Algoritmo_dell_amicizia {
     }
 
     public boolean bevanda() {
-    
-        return true;
+        Scanner input = new Scanner(System.in);
+        System.out.println("E di bere qualcosa di caldo?");
+        boolean risposta = input.nextBoolean();
+
+        if(risposta){
+            System.out.println("Scegli:\n" +
+                    "1. Tè\n" +
+                    "2. Caffè\n" +
+                    "3. Cioccolata");
+            int bevandaScelta = input.nextInt();
+            switch (bevandaScelta) {
+                case 1:
+                    System.out.println("Fatevi sto tè");
+                    break;
+                case 2:
+                    System.out.println("Fatevi sto caffè");
+                    break;
+                case 3:
+                    System.out.println("Fatevi sta cioccolata");
+                    break;
+            }
+            return true;
+        }
+        return false;
     }
 
     public void attivita() {

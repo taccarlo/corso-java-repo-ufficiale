d50789abce1dd4eda6444c48e247a32609a65439
edo2313 <32812884+edo2313@users.noreply.github.com>
2022-12-07 09:28:58 +0100

    Create branch gruppo_02

---
+++ lezione_7_12/gruppo_02/Algoritmo_dell_amicizia.java
@@ -0,0 +1,26 @@
+package esercitazione;
+
+import java.util.Scanner;
+
+public class Algoritmo_dell_amicizia {
+    public boolean incipit() {
+
+        return true;
+
+    }
+
+    public void finale() {
+        System.out.println("Siete diventati amici :)\n" +
+                "Ora hai una persona in più a cui rompere le palle in caso di bisogno, e viceversa");
+    }
+
+    public boolean bevanda() {
+    
+        return true;
+    }
+
+    public void attivita() {
+        
+
+    }
+}

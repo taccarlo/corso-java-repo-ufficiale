9139282e4a31acb412da078c4e1d6b88b391ea3b
MikeShady <86685382+MikeShady@users.noreply.github.com>
2022-12-07 09:45:39 +0100

    incipit function

--- lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
+++ lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class Algoritmo_dell_amicizia {
     public boolean incipit() {
 
-        return true;
+        System.out.println("Componi il numero della persona");
+        Scanner input = new Scanner(System.in);
+        String numero = input.nextLine();
+        System.out.println("E' in casa?");
+        boolean persona = input.nextBoolean();
+        if(!persona) {
+            System.out.println("Lascia un messaggio \n" +
+                    " aspetta di essere richiamato");
+        }
+        System.out.println("Ti va di mangiare qualcosa insieme?");
+        boolean risposta = input.nextBoolean();
+
+        if(risposta){
+            System.out.println("Mangiate qualcosa insieme");
+        }
+
+        return risposta;
 
     }
 

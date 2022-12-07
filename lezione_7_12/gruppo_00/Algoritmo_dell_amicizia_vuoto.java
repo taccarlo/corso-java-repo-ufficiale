0e76fe57d92cdd73a3e5aeb5f82c974960df7103
fcaneo00 <filippo.caneo@gmail.com>
2022-12-07 09:34:32 +0100

    function attivita

--- lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
+++ lezione_7_12/gruppo_00/Algoritmo_dell_amicizia_vuoto.java
@@ -20,7 +20,22 @@ public class Algoritmo_dell_amicizia {
     }
 
     public void attivita() {
-        
-
+        System.out.println("cos' altro ti va di fare?");
+        Scanner input = new Scanner(System.in);
+        //not used
+        input.nextLine();
+        int n = 0;
+        while (n<=6){
+            System.out.println("è una cosa che va di fare anche a te?");
+            boolean a = input.nextBoolean();
+            if (a) {
+                System.out.println("Scegli fra tutte le opzioni quella che ti sembra essere la meno disumana \n " +
+                        "fattela piacere");
+                break;
+            }
+            n++;
+        }
+        if(n<=6)
+            System.out.println("E facciamolo insieme dai");
     }
 }

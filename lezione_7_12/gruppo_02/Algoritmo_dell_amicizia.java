ddee7698458e29d46234989fa2daa9c21a176174
edo2313 <32812884+edo2313@users.noreply.github.com>
2022-12-07 09:33:51 +0100

    incipit 02

--- lezione_7_12/gruppo_02/Algoritmo_dell_amicizia.java
+++ lezione_7_12/gruppo_02/Algoritmo_dell_amicizia.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Algoritmo_dell_amicizia {
     public boolean incipit() {
+        System.out.println("Componi il numero della persona");
+        Scanner input = new Scanner(System.in);
+        String numero = input.nextLine();
+        System.out.println("E' in casa?");
+        boolean persona = input.nextBoolean();
+        if (!persona) {
+            System.out.println("Lascia un messaggio \n" +
+                    " aspetta di essere richiamato");
+        }
+        System.out.println("Ti va di mangiare qualcosa insieme?");
+        boolean risposta = input.nextBoolean();
 
-        return true;
+        if (risposta) {
+            System.out.println("Mangiate qualcosa insieme");
+        }
 
+        return risposta;
     }
 
     public void finale() {
@@ -15,12 +29,12 @@ public class Algoritmo_dell_amicizia {
     }
 
     public boolean bevanda() {
-    
+
         return true;
     }
 
     public void attivita() {
-        
+
 
     }
 }

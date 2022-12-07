448b95f1db86158d27bfb6fd773c5f194d28bc10
tommasobatt22 <tommasobattistoni22@gmail.com>
2022-12-07 11:11:19 +0100

    Added funzione02

--- lezione_7_12/gruppo_03/Algoritmo_della_vita.java
+++ lezione_7_12/gruppo_03/Algoritmo_della_vita.java
@@ -2,44 +2,55 @@ package lezione_7_12.gruppo_03;
 
 public class Algoritmo_della_vita {
 
-    public static void funzione01(){
-        System.out.println("La persona è nata!");
-        System.out.println("La persona segue le elementari e le medie!");
+    public static void funzione01() {
+        // Inizio vita
+        System.out.println("Il bro è nato!");
+        System.out.println("Il bro segue le elementari e le medie!");
         int n_bocciature = 0;
         int anno_superiori = 1;
-        while(true){
-            System.out.println("La persona è al " + anno_superiori + " delle superiori");
-            int roll_bocciatura = (int)(Math.random() * 100);
+        while (true) {
+            System.out.println("Il bro è al " + anno_superiori + " delle superiori");
+            int roll_bocciatura = (int) (Math.random() * 100);
             System.out.println(roll_bocciatura);
-            if(roll_bocciatura <= 40){
+            if (roll_bocciatura <= 40) {
                 n_bocciature++;
-                System.out.println("La persona è bocciata! Numero di bocciature: " + n_bocciature);
+                System.out.println("Il bro è bocciata! Numero di bocciature: " + n_bocciature);
             } else {
-                System.out.println("La persona ha superato il " + (anno_superiori + 1) + " anno!");
+                System.out.println("Il bro ha superato il " + (anno_superiori + 1) + " anno!");
                 anno_superiori++;
             }
-            if(n_bocciature >= 3){
-                System.out.println("La persona è stata bocciata troppi volte e non può proseguire gli studi!");
+            if (n_bocciature >= 3) {
+                System.out.println("Il bro è stata bocciata troppi volte e non può proseguire gli studi!");
                 break;
             }
-            if(anno_superiori == 5){
-                System.out.println("La persona ha superato gli studi superiori!");
+            if (anno_superiori == 5) {
+                System.out.println("Il bro ha superato gli studi superiori!");
                 break;
             }
             funzione02();
         }
     }
 
+    public static void funzione02() {
+        // Lavoro
+        System.out.println("Il bro è stato assunto 🤓");
+        int roll_infortunio = (int) (Math.random() * 100);
+        int n = 0;
+        while (n >= 20) {
+            if (roll_infortunio <= 20) {
+                System.out.println("Il bro è in ospedale, speriamo starà meglio🙏 ");
+                funzione03(true);
+            }
+        }
+        System.out.println("Il bro è sopravvissuto 🎉");
+        funzione03(false);
+    }
 
-    public static void funzione02(){
+    public static void funzione03(boolean infortunio) {
 
     }
 
-    public static void funzione03(){
-
-    }
-
-    public static void main(String args[]){
+    public static void main(String args[]) {
         funzione01();
     }
 }

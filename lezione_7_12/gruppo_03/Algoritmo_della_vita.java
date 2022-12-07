05b084de9da2b28f6e1b967d0088386c7409fd2b
Lorenzo <99982085+LorenzoC0@users.noreply.github.com>
2022-12-07 10:34:09 +0100

    Added funzione01

--- lezione_7_12/gruppo_03/Algoritmo_della_vita.java
+++ lezione_7_12/gruppo_03/Algoritmo_della_vita.java
@@ -1,5 +1,41 @@
 package lezione_7_12.gruppo_03;
 
 public class Algoritmo_della_vita {
-    
+
+    public static void funzione01(){
+        System.out.println("La persona è nata!");
+        System.out.println("La persona segue le elementari e le medie!");
+        int n_bocciature = 0;
+        int anno_superiori = 1;
+        while(true){
+            System.out.println("La persona è al " + anno_superiori + " delle superiori");
+            int roll_bocciatura = (int)Math.random() * 100;
+            if(roll_bocciatura <= 20){
+                n_bocciature++;
+                System.out.println("La persona è bocciata! Numero di bocciature: " + n_bocciature);
+            }
+            if(n_bocciature >= 3){
+                System.out.println("La persona è stata bocciata troppi volte e non può proseguire gli studi!");
+                break;
+            }
+            if(anno_superiori == 5){
+                System.out.println("La persona ha superato gli studi superiori!");
+                break;
+            }
+            funzione02();
+        }
+    }
+
+
+    public static void funzione02(){
+
+    }
+
+    public static void funzione03(){
+
+    }
+
+    public static void main(String args[]){
+        funzione01();
+    }
 }

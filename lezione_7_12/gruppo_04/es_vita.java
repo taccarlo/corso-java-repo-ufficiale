fb4d9df877fc979f766fc4ff4d13b4e9da2ac457
P3C1C <80387922+P3C1C@users.noreply.github.com>
2022-12-07 10:19:58 +0100

    creato es vita

---
+++ lezione_7_12/gruppo_04/es_vita.java
@@ -0,0 +1,13 @@
+package lezione_7_12.gruppo_04;
+
+public class es_vita {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void primaFase() {
+        int n_bocciature = 0; 
+        int anno_superato = 0;
+        System.out.println("La persona segue le elementari e le medie");
+    }
+}

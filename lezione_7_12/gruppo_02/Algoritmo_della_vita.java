b9f6f661b4a48d99a3a168ad8adf212140504618
edo2313 <32812884+edo2313@users.noreply.github.com>
2022-12-07 10:37:30 +0100

    start Algoritmo_della_vita gruppo02

---
+++ lezione_7_12/gruppo_02/Algoritmo_della_vita.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Algoritmo_della_vita {
+    public static void main(String[] args) {
+
+    }
+
+    public static void funzione1() {
+        Scanner input = new Scanner(System.in);
+    }
+}

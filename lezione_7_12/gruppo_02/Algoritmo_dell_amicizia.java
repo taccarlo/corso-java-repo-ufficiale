76fd4564d7c7af7c0870e2273b36b4a404b81680
GiovanniMosele <giovannimosele54@gmail.com>
2022-12-07 09:44:19 +0100

    esericizio_bevanda

--- lezione_7_12/gruppo_02/Algoritmo_dell_amicizia.java
+++ lezione_7_12/gruppo_02/Algoritmo_dell_amicizia.java
@@ -15,12 +15,33 @@ public class Algoritmo_dell_amicizia {
     }
 
     public boolean bevanda() {
-    
+        Scanner input = new Scanner(System.in);
+        System.out.println("E di bere qualcosa di caldo?");
+        boolean risposta = input.nextBoolean();
         return true;
+        if (risposta) {
+            System.out.println("Scegli:\n" +
+                    "1. Tè\n" +
+                    "2. Caffè\n" +
+                    "3. Cioccolata");
+            int bevandaScelta = input.nextInt();
+            switch (bevandaScelta) {
+                case 1:
+                    System.out.println("Fatevi sto tè");
+                    break;
+                case 2:
+                    System.out.println("Fatevi sto caffè");
+                    break;
+                case 3:
+                    System.out.println("Fatevi sta cioccolata");
+                    break;
+            }
+            return true;
+        }
+        return false;
     }
 
     public void attivita() {
-        
 
     }
 }

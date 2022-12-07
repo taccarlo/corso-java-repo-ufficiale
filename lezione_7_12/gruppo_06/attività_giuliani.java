35416bc3adb3a0018bd6264cdc3a8f23405be176
P3C1C <80387922+P3C1C@users.noreply.github.com>
2022-12-07 10:12:50 +0100

    Revert "Merge branch 'gruppo_06'"
    This reverts commit 9a90bda9f13f010cb4e3dcf99de4a1c0578e430e, reversing
    changes made to ce27e35c7d5ffa379eae2836eb8a9f2e4743f335.

--- lezione_7_12/gruppo_06/attività_giuliani.java
+++ lezione_7_12/gruppo_06/attività_giuliani.java
@@ -1,24 +1,5 @@
 package lezione_7_12.gruppo_06;
 
 public class attività_giuliani {
-    public void attivita() {
-        System.out.println("cos' altro ti va di fare?");
-        Scanner input = new Scanner(System.in);
-        //not used
-        input.nextLine();
-        int n = 0;
-        while (n<=6){
-            System.out.println("è una cosa che va di fare anche a te?");
-            boolean a = input.nextBoolean();
-            if (a) {
-                System.out.println("Scegli fra tutte le opzioni quella che ti sembra essere la meno disumana \n " +
-                        "fattela piacere");
-                break;
-            }
-            n++;
-        }
-        if(n<=6)
-            System.out.println("E facciamolo insieme dai");
-
-    }
+    
 }
--- lezione_7_12/gruppo_06/bevanda_zanuso.java
+++ lezione_7_12/gruppo_06/bevanda_zanuso.java
@@ -1,37 +1,5 @@
 package lezione_7_12.gruppo_06;
 
-import java.util.Scanner;
-
-
 public class bevanda_zanuso {
-    public void main(String[] args) {
-        bevanda();
-    }
-
-    public boolean bevanda() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("E di bere qualcosa di caldo?");
-        boolean risposta = input.nextBoolean();
-
-        if(risposta){
-            System.out.println("Scegli:\n" +
-                    "1. Tè\n" +
-                    "2. Caffè\n" +
-                    "3. Cioccolata");
-            int bevandaScelta = input.nextInt();
-            switch (bevandaScelta) {
-                case 1:
-                    System.out.println("Fatevi sto tè");
-                    break;
-                case 2:
-                    System.out.println("Fatevi sto caffè");
-                    break;
-                case 3:
-                    System.out.println("Fatevi sta cioccolata");
-                    break;
-            }
-            return true;
-        }
-        return false;
-    }
+    
 }

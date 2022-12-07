8fa78e8eee7c0da8999ed4b94ee6cf13e2f97fa0
fcaneo00 <filippo.caneo@gmail.com>
2022-12-07 11:22:25 +0100

    function lavoro

--- lezione_7_12/gruppo_01/Algoritmo_della_vita.java
+++ lezione_7_12/gruppo_01/Algoritmo_della_vita.java
@@ -2,20 +2,27 @@ import java.util.Scanner;
 
 public class Algoritmo_della_vita {
     public static void main(String[] args) {
-        System.out.println("Ops sei nato!");
+        System.out.println("Ops sei nato!\n");
         scuola();
-        lavoro();
+        if(lavoro()){
+            if (ultimi_anni_lavoro()) {
+                System.out.println("Sei il presidente mega galattico della federazione internazionale del lavoro pulito!\n");
+            } else {
+                System.out.println("Hai finito la tua carriera da operaio sfruttato!\n");
+            }
+        }
+
+        System.out.println("Si va in pensione");
     }
 
     public static boolean scuola() {
-        System.out.println("Sono stato bullizzato sia alle elementari dalle maestre... anche alle medie! :(");
-        System.out.print("Quante volte sei stato bocciato? ");
+        System.out.println("Sono stato bullizzato alle elementari dalle maestre... anche alle medie! :(\n");
         Scanner input = new Scanner(System.in);
         int n_bocciature = 0;
         int anno_superiori = 1;
 
-        while (anno_superiori != 6 || n_bocciature == 4) {
-            System.out.println("Seguo il " + anno_superiori + "° anno delle superiori");
+        while (anno_superiori != 6 || n_bocciature > 3) {
+            System.out.println("Seguo il " + anno_superiori + "' anno delle superiori");
             System.out.print("Sei stato bocciato? 0: NO - 1: SI ");
             int risposta = input.nextInt();
             if (risposta == 0) {
@@ -25,26 +32,44 @@ public class Algoritmo_della_vita {
             }
         }
 
+        System.out.println("");
         return true;
     }
 
     public static boolean lavoro() {
         System.out.println("Ho trovato un lavoro.. che schifo!");
         Scanner input = new Scanner(System.in);
-        int anno_lavoro = 0;
+        int anno_lavoro = 1;
 
-        while (anno_lavoro != 20) {
-            System.out.println("Sono al " + anno_lavoro + "° anno di lavoro, me ne restano " + (20 - anno_lavoro));
+        while (anno_lavoro < 21) {
+            System.out.println("Sono al " + anno_lavoro + "' anno di lavoro, me ne restano " + (20 - anno_lavoro));
             System.out.print("Hai subito un infortunio debilitante? 0: NO - 1: SI ");
             int risposta = input.nextInt();
 
             if (risposta == 0) {
                 anno_lavoro += 1;
             } else {
-                return true;
+                System.out.println("Mi disp 🤕 ma anche no godo!");
+                return false;
             }
         }
 
-        return false;
+        System.out.println("");
+        return true;
+    }
+
+    public static boolean ultimi_anni_lavoro() {
+        System.out.println("Manca poco ce la possiamo fare!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Quante promozioni hai ricevuto? ");
+        int promozioni = input.nextInt();
+
+        if (promozioni > 3) {
+            System.out.println("");
+            return true;
+        } else {
+            System.out.println("");
+            return false;
+        }
     }
 }

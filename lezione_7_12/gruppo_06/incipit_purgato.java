cd47244808f2fb16401ec7cfbb50793c655241e3
Nicolle Purgato <purgatonicolle@gmail.com>
2022-12-07 09:59:04 +0100

    incipit purgato

--- lezione_7_12/gruppo_06/incipit_purgato.java
+++ lezione_7_12/gruppo_06/incipit_purgato.java
@@ -1,5 +1,26 @@
 package lezione_7_12.gruppo_06;
+import java.util.Scanner;
 
 public class incipit_purgato {
-    
+    public boolean incipit(){
+        System.out.println("Componi il numero di telefono della persona");
+        Scanner input = new Scanner(System.in);
+        String numerotel = input.nextLine();
+        boolean persona = input.nextBoolean();
+        if (!persona){
+            System.out.println("Lascia un messaggio, aspetta di essere richiamato");   
+        }
+            System.out.println("Ti va di mangiare qualcosa insieme?");
+            boolean risposta = input.nextBoolean();
+
+        if (risposta){
+                System.out.println("Mangiate qualcosa insieme");
+        }
+        
+        return risposta;
+        
+
+        
+
+    }
 }

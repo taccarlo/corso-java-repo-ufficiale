cea22c3aba99846a3a6d9b7ff6cf1f2ecc9ce1aa
Leonardo Rossi <leonardorossi098@gmail.com>
2022-12-07 10:46:41 +0100

    funzione3

--- lezione_7_12/gruppo_02/Algoritmo_della_vita.java
+++ lezione_7_12/gruppo_02/Algoritmo_della_vita.java
@@ -8,4 +8,15 @@ public class Algoritmo_della_vita {
     public static void funzione1() {
         Scanner input = new Scanner(System.in);
     }
+
+    public static void funzione3() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("quante promozioni hai avuto?");
+        int a = input.nextInt();
+        if(a > 3){
+            System.out.println("grande bro, sei diventato il capo");
+        }else{
+            System.out.println("bro mi dispiace, vai a fare il pensionato");
+        }
+    }
 }

dfb47a10f49475b5889693514ea5f38415d57a59
Francesco Fiorio <fiorio.francesco.200182@gmail.com>
2022-12-07 09:41:24 +0100

    COSE

--- lezione_7_12/gruppo_05/classe.java
+++ lezione_7_12/gruppo_05/classe.java
@@ -1,5 +1,25 @@
-public class classe{
-    public static void main(String[] args) {
-        
+package lezione_7_12.gruppo_05;
+
+import java.util.Scanner;
+
+public class Algoritmo_dell_amicizia {
+    public boolean incipit() {
+
+        return true;
+
+    }
+
+    public void finale() {
+        System.out.println("Siete diventati amici :)\n" +
+                "Ora hai una persona in più a cui rompere le palle in caso di bisogno, e viceversa");
+    }
+
+    public boolean bevanda() {
+
+        return true;
+    }
+
+    public void attivita() {
+
     }
 }

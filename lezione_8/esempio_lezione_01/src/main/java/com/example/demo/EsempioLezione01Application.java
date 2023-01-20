2b28fb6d0b37842c49a06f5ade9c6fb3bf47bfda
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-01-20 12:46:57 +0100

    [ADD] progetto fatto in classe

---
+++ lezione_8/esempio_lezione_01/src/main/java/com/example/demo/EsempioLezione01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsempioLezione01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsempioLezione01Application.class, args);
+	}
+
+}

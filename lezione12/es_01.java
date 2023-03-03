c787c261ba6dbed976a296f6ad81dffa6784c8a1
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-03-03 11:35:43 +0100

    [UP] example of usage of constant variable

---
+++ lezione12/es_01.java
@@ -0,0 +1,10 @@
+package lezione12;
+public class es_01 {
+
+    final static int count = 34;
+    public static void main(String[] args) {
+        System.out.println("Ciao");
+        //errore
+        count = 50;
+    }
+}
